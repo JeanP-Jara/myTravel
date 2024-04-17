0cbfa05f67f3f7ad071b3643e3263412405e9d99
JeanP-Jara <jeanjara.15926@gmail.com>
2024-04-16 19:45:33 -0500

    clase

---
+++ myTravel/footer.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tours</title>
+</head>
+<body>
+    <footer>
+        <div class="contenedor-footer">
+            <div class="seccion-footer">
+                <h2>Sobre Nosotros</h2>
+                <p>Descripción breve de la empresa o sitio web.</p>
+            </div>
+            <div class="seccion-footer">
+                <h2>Enlaces Rápidos</h2>
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Servicios</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </div>
+            <div class="seccion-footer">
+                <h2>Contacto</h2>
+                <p>Dirección: 123 Calle Principal, Ciudad</p>
+                <p>Teléfono: 555-123-456</p>
+                <p>Email: info@example.com</p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
--- myTravel/index.php
+++ myTravel/index.php
@@ -11,46 +11,9 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="js/script.js"></script>
 </head>
-<body>
-    <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#inicio" >Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" onclick="cargarContenido('pages/tours.php')">Tours</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-                </div>
-            </div>
-        </nav>
-    </header>
-    <main id="contenido">
+<body>    
+    <?php include('header.php'); ?>
+    <main>
         <div id="inicio" style="height: 850px; width: 100%;">
             <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-bs-ride="carousel">
                 <div class="carousel-indicators">
@@ -165,29 +128,8 @@
         <div id="contacto">
             <!-- Contenido de la sección Contacto -->
         </div>
-    </main>
-    <footer>
-        <div class="contenedor-footer">
-            <div class="seccion-footer">
-                <h2>Sobre Nosotros</h2>
-                <p>Descripción breve de la empresa o sitio web.</p>
-            </div>
-            <div class="seccion-footer">
-                <h2>Enlaces Rápidos</h2>
-                <ul>
-                    <li><a href="#">Inicio</a></li>
-                    <li><a href="#">Servicios</a></li>
-                    <li><a href="#">Contacto</a></li>
-                </ul>
-            </div>
-            <div class="seccion-footer">
-                <h2>Contacto</h2>
-                <p>Dirección: 123 Calle Principal, Ciudad</p>
-                <p>Teléfono: 555-123-456</p>
-                <p>Email: info@example.com</p>
-            </div>
-        </div>
-    </footer>
+    </main>    
+    <?php include('footer.php'); ?>
     <script src="js/script.js"></script>
 </body>
 </html>
